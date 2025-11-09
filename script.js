c4f1de5ff94f72a9c649ce8ff03c63684d83037f
Nischal <123059530+sarthakkc36@users.noreply.github.com>
2025-11-09 17:33:05 +0545

    Week 2

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeFontSize() {
+    document.getElementById('box1').style.fontSize = '24px';
+}
+
+function changeStyle() {
+    document.getElementById('box3').style.fontStyle = 'italic';
+}
+
+function changeImage() {
+    document.getElementById('box5').style.backgroundImage = 'url(tiger.jpg)';
+    document.getElementById('box5').style.backgroundSize = 'cover';
+}
+
+function addText() {
+    document.getElementById('box7').textContent = 'THIS IS A NEW TEXT';
+}
