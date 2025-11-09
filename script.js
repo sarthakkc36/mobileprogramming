1f91d054dcb84026c4f7d6986bf781db1c73ad88
Nischal <123059530+sarthakkc36@users.noreply.github.com>
2025-11-09 17:38:59 +0545

    Week 2

--- script.js
+++ script.js
@@ -14,3 +14,50 @@ function changeImage() {
 function addText() {
     document.getElementById('box7').textContent = 'THIS IS A NEW TEXT';
 }
+function addNumbers() {
+    var a = 5;
+    var b = 8;
+    var c = a + b;
+    return c;
+}
+
+function subtractNumbers() {
+    var a = 5;
+    var b = 8;
+    var c = a - b;
+    return c;
+}
+
+function multiplyNumbers() {
+    var a = 5;
+    var b = 8;
+    var c = a * b;
+    return c;
+}
+
+function divideNumbers() {
+    var a = 5;
+    var b = 8;
+    var c = a / b;
+    return c;
+}
+
+function displaySum() {
+    var result = addNumbers();
+    document.getElementById('result').textContent = 'The sum is: ' + result;
+}
+
+function displaySubtraction() {
+    var result = subtractNumbers();
+    document.getElementById('result').textContent = 'The subtraction is: ' + result;
+}
+
+function displayMultiplication() {
+    var result = multiplyNumbers();
+    document.getElementById('result').textContent = 'The multiplication is: ' + result;
+}
+
+function displayDivision() {
+    var result = divideNumbers();
+    document.getElementById('result').textContent = 'The division is: ' + result;
+}
